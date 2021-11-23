d31581ea8fba10c0da52c847d059dacd1b5dd032
Rafael Viana <rafaelviana544@yahoo.com>
2021-11-23 12:50:03 -0300

    projeto criado

---
+++ AppGame/AppGame/Model/GameScore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppGame.Model
+{
+    internal class GameScore
+    {
+    }
+}
---
+++ AppGame/AppGame/Services/GameScoreApi.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppGame.Services
+{
+    internal class GameScoreApi
+    {
+    }
+}
